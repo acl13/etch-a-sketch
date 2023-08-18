e01c1a09b9ec6a94ea80bf1c1a8e4ffa54642f55
acl13 <128934431+acl13@users.noreply.github.com>
2023-08-18 14:48:53 -0400

    change container id to canvas

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ console.log("Hello, world");
 
 
 function createGrid() {
-    const container = document.getElementById('container');
-    container.style.gridTemplateColumns = `repeat(16, 1fr)`
-    container.style.gridTemplateRows = `repeat(16, 1fr)`
+    const canvas = document.getElementById('canvas');
+    canvas.style.gridTemplateColumns = `repeat(16, 1fr)`
+    canvas.style.gridTemplateRows = `repeat(16, 1fr)`
     for(let i = 0; i < 256; i++){
 
         let box = document.createElement('div');
-        container.appendChild(box);
+        canvas.appendChild(box);
     }}
     
     createGrid();
