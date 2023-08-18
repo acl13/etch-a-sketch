721fcba69f7eb948c98b581a45141d63b084b9fe
acl13 <128934431+acl13@users.noreply.github.com>
2023-08-18 14:45:17 -0400

    create 16 x 16 grid

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 console.log("Hello, world");
 
-const container = document.getElementById('container');
-console.log(container);
-
 
 
 function createGrid() {
-    for(let i = 0; i < 100; i++){
+    const container = document.getElementById('container');
+    container.style.gridTemplateColumns = `repeat(16, 1fr)`
+    container.style.gridTemplateRows = `repeat(16, 1fr)`
+    for(let i = 0; i < 256; i++){
 
         let box = document.createElement('div');
-        box.textContent = "test";
-        container.append(box);
+        container.appendChild(box);
     }}
     
     createGrid();
