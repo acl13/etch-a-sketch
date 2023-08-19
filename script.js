1116efe1c71e79b4c87b65dc1c13e6944b024f3f
acl13 <128934431+acl13@users.noreply.github.com>
2023-08-19 12:50:32 -0400

    make grid size respond to user input

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ console.log("Hello, world");
 
 
 function createGrid() {
-    const canvas = document.getElementById('canvas');
-    canvas.style.gridTemplateColumns = `repeat(16, 1fr)`
-    canvas.style.gridTemplateRows = `repeat(16, 1fr)`
-    for(let i = 0; i < 256; i++){
+    const size = prompt("Grid Rows/Colums (max 100)");
+    if(size < 100 && size > 0){
+        const canvas = document.getElementById('canvas');
+        const grid = (size * size)
+        canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+        canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`
+        for(let i = 0; i < grid; i++){
 
-        let box = document.createElement('div');
-        canvas.appendChild(box);
+            let box = document.createElement('div');
+            canvas.appendChild(box);
+            console.log(size);
+         }
+    } else {
+        alert("Oops! Enter a number between 1 and 100");
     }}
     
-    createGrid();
 
+
+
+const newGrid = document.getElementById('newGrid');
+newGrid.addEventListener('click', createGrid);
 
