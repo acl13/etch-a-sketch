7250f5405673dc4404c0b346ed800b553e3979c9
acl13 <128934431+acl13@users.noreply.github.com>
2023-08-21 19:18:52 -0400

    populate 16 X 16 grid when page loads

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log("Hello, world");
 
+const canvas = document.getElementById('canvas');
+
 
 function createGrid() {
     const size = prompt("Grid Rows/Colums (max 100)");
     if(size < 101 && size > 0){
-        const canvas = document.getElementById('canvas');
+       // const canvas = document.getElementById('canvas');
         canvas.innerHTML = '';
         const grid = (size * size)
         canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -18,13 +20,25 @@ function createGrid() {
                 box.classList.add('hover');
             } );
             console.log(size);
-         }
-     
+         } 
     } else {
         alert("Oops! Enter a number between 1 and 100");
     }}
     
-createGrid();
+function createInitialGrid (){
+    canvas.style.gridTemplateColumns = `repeat(16, 1fr)`
+    canvas.style.gridTemplateRows = `repeat(16, 1fr)`
+    for(let i = 0; i < 256; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('box');
+        canvas.appendChild(cell);
+        cell.addEventListener('mouseover',  function(){
+            cell.classList.add('hover');
+        });
+     }
+}  
+
+createInitialGrid();
 
 
 const newGrid = document.getElementById('newGrid');
