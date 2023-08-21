aa801008c5b04ae43b8712ef040dbdb72c83dc9a
acl13 <128934431+acl13@users.noreply.github.com>
2023-08-21 19:48:55 -0400

    clean code

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-console.log("Hello, world");
-
 const canvas = document.getElementById('canvas');
 
 
 function createNewGrid() {
     const size = prompt("Grid Rows/Colums (max 100)");
     if(size < 101 && size > 0){
-       // const canvas = document.getElementById('canvas');
         canvas.innerHTML = '';
         const grid = (size * size)
         canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
         canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`
         for(let i = 0; i < grid; i++){
-
             let box = document.createElement('div');
             box.classList.add('grid');
             canvas.appendChild(box);
             box.addEventListener('mouseover',  function () {
-                //box.classList.add('hover');
                 let colors = ['#ff0000', '#00ff00', '#0000ff', '#ff3333', '#ffff00', '#ff6600'];
                 let randomColor = colors[(Math.floor(Math.random() * colors.length))];
                 box.style.backgroundColor = randomColor;
            } );
-            console.log(size);
          } 
     } else {
         alert("Oops! Enter a number between 1 and 100");
@@ -47,10 +41,5 @@ createInitialGrid();
 const newGrid = document.getElementById('newGrid');
 newGrid.addEventListener('click', createNewGrid);
 
-function pickColor() {
-    let colors = ['#ff0000', '#00ff00', '#0000ff', '#ff3333', '#ffff00', '#ff6600'];
-    let randomColor = colors[(Math.floor(Math.random() * colors.length))];
-    let rainbowCell = document.getElementsByClassName('grid');
-    rainbowCell.style.color = randomColor;
-}
+
 
